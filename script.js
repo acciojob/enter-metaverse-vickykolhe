9d27fd4fbb768f92a6a6d91e544c74aaa7a79b6b
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-08-07 09:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let ptag=document.getElementById("status");
+let btntag=document.getElementById("enterBtn");
+enterBtn.addEventListener("click",()=>{
+	ptag.innerHTML="<h1>Entered Metaverse</h1>"
+})
